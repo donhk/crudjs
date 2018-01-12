e4dbd21949b2bf096f669f924197aaacb4693d2f
donhk <hk.fred@hotmail.com>
2018-01-11 22:21:51 -0500

    added mongo code

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const bodyParser= require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, function() {
-  console.log('listening on 3000');
-  app.get('/', (req, res) => {
-  	res.sendFile(__dirname + '/index.html');
-	});
+var db
+MongoClient.connect('link-to-mongodb', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+      console.log('listening on 3000')
+      app.get('/', (req, res) => {
+            res.sendFile(__dirname + '/index.html');
+          });
+     app.post('/quotes', (req, res) => {
+        console.log('Hellooooooooooooooooo!')
+        console.log(req.body)
+     });
+  });
 });
 
 console.log('May Node be with you');
